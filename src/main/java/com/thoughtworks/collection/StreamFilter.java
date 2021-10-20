504f397df9f0887d11cdc41046bfd0e59531e171
vincent <luk96515@gmail.com>
2021-10-20 11:05:04 +0800

    [StreamCollection][vincent] init:Initial commit

---
+++ src/main/java/com/thoughtworks/collection/StreamFilter.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.collection;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilter {
+
+    public List<Integer> filterEven(List<Integer> numbers) {
+        return null;
+    }
+
+    public List<String> filterEndWithS(List<String> words) {
+        return null;
+    }
+
+    public List<Integer> getCommonElements(List<Integer> numbers, List<Integer> anotherNumbers) {
+        return null;
+    }
+
+
+}
