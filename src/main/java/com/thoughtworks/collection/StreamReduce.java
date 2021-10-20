4d82cd2dfd2cb276517ed90444d33c67ef75841b
vincent <luk96515@gmail.com>
2021-10-20 14:36:50 +0800

    [StreamCollection][Vincent] feat:stream update get last odd

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -5,17 +5,18 @@ import java.util.List;
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-return 0;
-//        return returnnumbers.stream().reduce();
+
+        return numbers.stream().filter(number ->number %2==1 ).reduce((first, second)->second).orElse(0);
+
     }
 
     public String getLongest(List<String> words) {
 
-        return words.stream().reduce("",(word1, word2) -> word1.length() > word2.length() ? word1 : word2);
+        return words.stream().reduce("", (word1, word2) -> word1.length() > word2.length() ? word1 : word2);
     }
 
     public int getTotalLength(List<String> words) {
 
-        return words.stream().reduce("",(word1,word2) -> word1 + word2).length();
+        return words.stream().reduce("", (word1, word2) -> word1 + word2).length();
     }
 }
